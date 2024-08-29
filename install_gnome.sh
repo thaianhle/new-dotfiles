a378295fde865e3092a3ccd39cfa1e2a5ef0e730
thaianhle <149495717+thaianhle@users.noreply.github.com>
2024-08-29 04:50:10 +0000

    new source

---
+++ install_gnome.sh
@@ -0,0 +1,22 @@
+set -e
+
+gnome() {
+    sudo apt install -y \
+        eog \
+        evince \
+        gnome-calculator \
+        gnome-disk-utility \
+        gnome-screenshot \
+        gnome-session \
+        gnome-shell-extensions \
+        gnome-system-monitor \
+        gnome-terminal \
+        gnome-tweaks \
+        nautilus \
+        nautilus-wipe \
+        network-manager-gnome \
+        network-manager-openvpn \
+        network-manager-openvpn-gnome \
+        wl-clipboard \
+        xsel
+}
